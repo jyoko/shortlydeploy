956a17db00e18d08e12f6d03f3c9801cad07ba43
jyoko <jyoko@users.noreply.github.com>
2015-09-18 01:43:39 -0400

    Visits bugfix
    Lines 119/120 of request-handler can be removed after this commit,
    just make sure all links listed have #Visits (including 0)

--- app/models/link.js
+++ app/models/link.js
@@ -13,6 +13,7 @@ urlSchema.pre('save', function(next) {
   var shasum = crypto.createHash('sha1');
   shasum.update(this.url);
   this.code = shasum.digest('hex').slice(0,5);
+  this.visits = this.visits||0;
   next();
 });
 
